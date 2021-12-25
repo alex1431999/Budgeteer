1b54b352bd777f60fa403b574d406c71f4f97698
alex1431999 <alexhaller99@yahoo.de>
2021-12-25 19:47:53 +0100

    feat(budgets): add budgets view

---
+++ src/components/Budget.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="budget">
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Budgets.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="Budgets">
+    Budgets
+  </div>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
