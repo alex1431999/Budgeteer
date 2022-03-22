5e9a6f177e8c61ba378867ac61d6d0372223b955
Alexander <alex1431999@gmail.com>
2022-03-22 11:39:30 +0100

    feat(client): express http proxy

--- client/server.js
+++ client/server.js
@@ -1,18 +1,15 @@
 /**
  * This is a js file so we don't have to transpile it just to run the server in production
  */
+require('dotenv').config();
+
 const express = require('express');
 const proxy = require('express-http-proxy');
-
-const apiProxy = proxy(process.env.VUE_APP_API_URL, {
-    proxyReqPathResolver: req => url.parse(req.baseUrl).path
-});
-
 const app = express();
 
 app.use(express.static('dist'));
-app.use('/api/*', apiProxy);
+app.use('/api', proxy(process.env.VUE_APP_API_URL));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.listen(port);
