3a9f444874094f7f6667e2acaf5956178657f456
alex1431999 <alex@baserow.io>
2023-10-20 20:40:21 +0200

    refactor(sheets): Write a generic method to add ids

--- server/database/queries/budgetSheets.ts
+++ server/database/queries/budgetSheets.ts
@@ -1,6 +1,6 @@
 import { getDb } from '../index';
 import { IBudgetSheet } from '../../types/Budget';
-import { addId } from '../../util/database';
+import { addId, addIdsToNestedObject } from '../../util/database';
 
 const getBudgetSheetsCollection = async () => {
   const db = await getDb();
@@ -26,19 +26,7 @@ export const setBudgetSheets = async (
   }
 
   // Add Ids to every sheet, group and budget
-  // TODO: we could write a generic function here that adds an id on every level of an
-  //  object
-  const budgetSheetsWithId = budgetSheets.map((sheet) => {
-    const sheetWithId = addId(sheet);
-    const budgetGroupsWithId = sheetWithId.budgetGroups?.map((group) => {
-      const budgetGroupWithId = addId(group);
-      const budgetsWithId = budgetGroupWithId.budgets?.map(addId);
-
-      return { ...budgetGroupWithId, budgets: budgetsWithId || [] };
-    });
-
-    return { ...sheetWithId, budgetGroups: budgetGroupsWithId || [] };
-  });
+  const budgetSheetsWithId = budgetSheets.map(addIdsToNestedObject);
 
   const collection = await getBudgetSheetsCollection();
 
--- server/util/database.ts
+++ server/util/database.ts
@@ -1,4 +1,5 @@
 import { ObjectId } from 'mongodb';
+import _ from 'lodash';
 
 interface ObjectWithId {
   _id: ObjectId
@@ -9,4 +10,20 @@ export function addId<T>(obj: T): T & ObjectWithId {
     _id: new ObjectId(),
     ...obj,
   };
+}
+
+export function addIdsToNestedObject<T>(obj: T): T & ObjectWithId {
+  const objWithId = addId(obj);
+
+  Object.keys(objWithId).forEach((key) => {
+    const value = objWithId[key];
+
+    if (_.isArray(value)) {
+      objWithId[key] = value.map(addIdsToNestedObject);
+    } else if (_.isObject(value) && !(value instanceof ObjectId)) {
+      objWithId[key] = addIdsToNestedObject(value);
+    }
+  });
+
+  return objWithId;
 }
